cb0aac269173745bf0d21ccb9c74837314135512
Aleksandr Gornostal <sanya.gornostal@gmail.com>
2017-06-07 21:26:02 +0300

    updated settings for staging env

--- build-utils/setup-remote-env.py
+++ build-utils/setup-remote-env.py
@@ -17,6 +17,23 @@ settings_f = open(os.path.join(root_path, 'zappa_settings.json'))
 settings = json.load(settings_f)
 (_, _, bucket, path) = settings[env]['remote_env'].split('/')
 
-s3.upload_file(os.path.join(root_path, 'remote_env.json'),
-               bucket,
-               path)
+
+remote_env = {
+    "AWS_DEFAULT_REGION": os.environ["AWS_DEFAULT_REGION"],
+
+    "AUTH0_DOMAIN": os.environ["AUTH0_DOMAIN"],
+    "AUTH0_CLIENT_ID": os.environ["AUTH0_CLIENT_ID"],
+    "AUTH0_CLIENT_SECRET": os.environ["AUTH0_CLIENT_SECRET"],
+
+    "EXTENSIONS_TABLE_NAME": os.environ["EXTENSIONS_TABLE_NAME"],
+    "EXT_IMAGES_BUCKET_NAME": os.environ["EXT_IMAGES_BUCKET_NAME"],
+
+    "LOG_LEVEL": os.environ.get("LOG_LEVEL", 20)
+}
+
+s3.put_object(
+    Body=json.dumps(remote_env, indent=2),
+    Bucket=bucket,
+    Key=path,
+    ContentEncoding='utf-8'
+)
