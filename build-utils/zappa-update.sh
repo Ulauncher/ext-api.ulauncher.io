baefb4ebc9f6f63308e5528aa4d93962e50b9a7d
Aleksandr Gornostal <al.gornostal@gmail.com>
2017-10-01 13:59:33 +0300

    minor fixes

--- build-utils/zappa-update.sh
+++ build-utils/zappa-update.sh
@@ -6,17 +6,25 @@ cd ..
 sed -i "s/commit =.*/commit = '$TRAVIS_COMMIT'/g" ext_api/config.py
 sed -i "s/deployed_on =.*/deployed_on = '`date`'/g" ext_api/config.py
 
+notify_deploy() {
+    echo
+    echo "====================="
+    echo "Deploying to $1"
+    echo "====================="
+    echo
+}
+
 cmd="./test tests"
 deploy="virtualenv /var/docker_env"
 deploy="$deploy && source /var/docker_env/bin/activate"
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-    echo "Deploying to prod"
+    notify_deploy prod
     cmd="$cmd && $deploy && zappa update prod"
 elif [ "$TRAVIS_BRANCH" == "stg" ]; then
-    echo "Deploying to staging"
+    notify_deploy staging
     cmd="$cmd && $deploy && zappa update stg"
 elif [ "$TRAVIS_BRANCH" == "dev" ]; then
-    echo "Deploying to dev"
+    notify_deploy dev
     cmd="$cmd && $deploy && zappa update dev"
 else
     echo "Run without deploy"
